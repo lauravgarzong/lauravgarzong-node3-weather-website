9a6588d37dcb1d3ad03050a458b9fc92a34a3e56
Laura Garzón <lvgarzong@unal.edu.co>
2021-09-18 20:17:53 -0500

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,38 @@
+console.log('Client side javascript file is loaded!')
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+//messageOne.textContent = 'From JavaScript'
+const messageTwo = document.querySelector('#message-2')
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault() // To prevent default behaviour of refreshing the browser
+    
+    const location =  search.value
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+        if(data.error) {
+            console.log(data.error)
+            messageOne.textContent = data.error
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+        }
+        
+    })
+})
+
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,35 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=80bbb571c5a3c76957938a7fe296e65b&query=' 
+    + latitude + ',' + longitude + '&units=f'
+     
+    request({url, json:true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error){
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature 
+            + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+        }
+    })
+
+}
+
+module.exports = forecast
+
+// London Weather Request
+// const url = 'http://api.weatherstack.com/current?access_key=80bbb571c5a3c76957938a7fe296e65b&query=51.5072,-0.1275&units=f'
+
+// request({ url: url, json: true }, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to weather service!')
+//     } else if (response.body.error) {
+//         console.log('Unable to find location')
+//     } else{
+//         console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature 
+//      + ' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.')
+//     }
+    
+// })
