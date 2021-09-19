6824652b2ae7b839f4c64b0c1eb7363c6a6c5070
Laura Garzón <lvgarzong@unal.edu.co>
2021-09-18 20:20:29 -0500

    Comment unnecessary console.log calls

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log('Client side javascript file is loaded!')
+//console.log('Client side javascript file is loaded!')
 
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //     response.json().then((data) => {
@@ -23,11 +23,11 @@ weatherForm.addEventListener('submit', (e) => {
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if(data.error) {
-            console.log(data.error)
+            //console.log(data.error)
             messageOne.textContent = data.error
         } else {
-            console.log(data.location)
-            console.log(data.forecast)
+            //console.log(data.location)
+            //console.log(data.forecast)
             messageOne.textContent = data.location
             messageTwo.textContent = data.forecast
         }
